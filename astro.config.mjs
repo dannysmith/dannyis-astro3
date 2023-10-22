d28821dfc3032d04385092113fda89e91718bb94
Danny Smith <hi@danny.is>
2023-10-22 21:34:09 +0100

    Clean up generated code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import prefetch from "@astrojs/prefetch";
+import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), prefetch()]
-});
+  integrations: [mdx(), sitemap(), prefetch()],
+  redirects: {
+    '/meeting':
+      'https://dannysmith.notion.site/Book-a-Meeting-with-Danny-e39fc8def5514b67b559b2e5a51934ae?pvs=4',
+    '/tools': 'https://betterat.work/toolbox',
+  },
+})
