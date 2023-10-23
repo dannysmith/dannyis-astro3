2e67bd74586c72e7575b02488d4c8a8ac4c0d051
Danny Smith <hi@danny.is>
2023-10-23 01:30:20 +0100

    Add properties to posts and redirect for external artciles

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    isDraft: z.boolean().default(false),
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    description: z.string().optional(),
+    redirectURL: z.string().url().optional(),
+    platform: z.enum(['medium', 'external']).optional(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+})
 
-export const collections = { blog };
+export const collections = { blog }
