fbf967c0407bb9f72b478491e7c50c1ed370c5d3
Danny Smith <hi@danny.is>
2023-10-23 02:43:43 +0100

    Add styleguide, components and aliases

---
+++ src/components/components.ts
@@ -0,0 +1,7 @@
+// Use this file to import and export all components which are likely to be used in MDX files
+
+import Notion from './Notion.astro'
+import NotionBlock from './NotionBlock.astro'
+import Grid from './Grid.astro'
+
+export { Notion, NotionBlock, Grid }
