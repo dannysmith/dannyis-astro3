66b5605cc27f678ba617b80e7b30b99361951e0f
Danny Smith <hi@danny.is>
2024-05-28 18:33:03 +0100

    Fix RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,22 @@ import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
 
 export async function GET(context) {
-  const posts = await getCollection('blog')
+  const posts = await getCollection('blog', ({ data }) => {
+    return import.meta.env.PROD ? data.draft !== true : true
+  })
+
+  const notes = await getCollection('notes')
+
+  let all = posts.concat(notes)
+  all.sort((b, a) => a.data.pubDate.valueOf() - b.data.pubDate.valueOf())
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
+    items: all.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `/writing/${post.slug}/`,
     })),
   })
 }
